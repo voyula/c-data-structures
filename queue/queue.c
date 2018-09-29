634f6cc69ad07b6096087dfc8f7a9d0ebc7ba7f1
voyula <40173603+voyula@users.noreply.github.com>
2018-09-29 23:06:54 +0300

    Update queue.c

--- queue/queue.c
+++ queue/queue.c
@@ -46,7 +46,7 @@ int dequeue(struct Queue* queue)
         return INT_MIN;
     }
     int item = queue->array[queue->front];
-    queue->front = (queue->front + 1)%queue->capacity;
+    queue->front = (queue->front + 1) % queue->capacity;
     queue->size = queue->size - 1;
     return item;
 }
