8c0e309e79501b9ac09c59bb25f524c9271c00b3
Your Name <you@example.com>
2018-09-29 09:17:18 +0300

    update

--- README.md
+++ README.md
@@ -4,9 +4,5 @@
 | --- | --- |
 | [Stack](stack) | capacity, clear, isFull, isEmpty, peek, push, pop |
 | [LinkedList](linked-list) | undefined |
-
-To be added later;
-
-| Struct |  Methods |
-| --- | --- |
 | [Queue](queue) | undefined |
+| [Graph](graph) | undefined |
---
+++ algorithms/bubble-sort/bubble-sort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubbleSort(int sort[])
+{
+    for (int i = 0; i < 5; i++) {
+        for (int j = 1; j < 5 - i; j++) {
+            if (sort[j - 1] > sort[j]) {
+                int temp = sort[j];
+                sort[j] = sort[j - 1];
+                sort[j - 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", sort[i]);
+    }
+}
+
+int main()
+{
+    int sort[] = {1, 9, 5, 81, 10};
+    bubbleSort(sort);
+
+    return 0;
+}
